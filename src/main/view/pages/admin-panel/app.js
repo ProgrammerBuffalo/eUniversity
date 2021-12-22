260e41293af9c73de6c3445792118d36ea6d3a64
PNik1ta <75731861+PNik1ta@users.noreply.github.com>
2021-12-22 21:47:56 +0400

    Make admin-student page adaptive - done!

--- src/main/view/pages/admin-panel/app.js
+++ src/main/view/pages/admin-panel/app.js
@@ -1,9 +1,5 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-	 document.getElementById("main").style.marginLeft = "250px";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-		document.getElementById("main").style.marginLeft = "0";
-}
+$(document).ready(() => {
+	$(".brand-burger").click(() => {
+		$(".nav-side-menu").toggleClass("active");
+	})
+})
