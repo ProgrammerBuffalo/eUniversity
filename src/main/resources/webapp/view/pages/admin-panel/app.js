b0002448b6a43fdf5121e933dc34bf9e2bc31203
PNik1ta <75731861+PNik1ta@users.noreply.github.com>
2022-01-12 22:46:12 +0400

    Fix select in angular - done!

--- src/main/resources/webapp/view/pages/admin-panel/app.js
+++ src/main/resources/webapp/view/pages/admin-panel/app.js
@@ -1,9 +1,11 @@
 $(document).ready(() => {
-	initModal(document.getElementById('students-change'), 
-	document.getElementsByClassName("change-btn"),
-	 document.getElementById("closeStudentsChangeBtn"));
+	initModal(document.getElementById('students-change'),
+		document.getElementsByClassName("change-btn"),
+		document.getElementById("closeStudentsChangeBtn"));
 
-	 initModal(document.getElementById('students-add'), 
+	initModal(document.getElementById('students-add'),
 		document.getElementsByClassName("add-btn"),
-	 document.getElementById("closeStudentsAddBtn"));
+		document.getElementById("closeStudentsAddBtn"));
 })
+
+console.log(document.getElementById("select"));
