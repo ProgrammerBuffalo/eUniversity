3dc740096d50f0dad80ae53ab3dbe14142a74115
PNik1ta <75731861+PNik1ta@users.noreply.github.com>
2022-01-14 12:44:43 +0400

    Change admin schedule page - done!

--- src/main/resources/webapp/view/pages/admin-panel/app.js
+++ src/main/resources/webapp/view/pages/admin-panel/app.js
@@ -7,24 +7,3 @@ $(document).ready(() => {
 		document.getElementsByClassName("add-btn"),
 		document.getElementById("closeStudentsAddBtn"));
 })
-
-
-const newTr = `
-<tr >
-	<th contenteditable="true">Enter teacher</th>
-	<td contenteditable="true">Enter day</td>
-	<td contenteditable="true">Enter time</td>
-	<td class="td-red">
-		<div class="table-btn remove-btn">
-			<i class="fas fa-times"></i>
-		</div>
-	</td>
-</tr >`;
-
-const addBtns = document.getElementsByClassName('add-btn');
-
-Array.prototype.forEach.call(addBtns, (btn) => {
-	btn.addEventListener('click', function (e) {
-		this.closest('.table-block').querySelector('tbody').innerHTML += newTr;
-	})
-})
