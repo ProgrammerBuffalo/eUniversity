e50e4a84338a86e02158193ee4831d0aff68306d
PNik1ta <75731861+PNik1ta@users.noreply.github.com>
2022-01-22 20:53:06 +0400

    Fix home page - done!

--- src/main/resources/webapp/eUniversity/src/app/admin/home/home.component.ts
+++ src/main/resources/webapp/eUniversity/src/app/admin/home/home.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss', '../../shared/table-block.scss']
+  styleUrls: ['../../shared/table-block.scss', './home.component.scss',]
 })
 export class HomeComponent implements OnInit {
 
