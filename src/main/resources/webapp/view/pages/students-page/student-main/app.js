c8585182342debdc9dd340a7b5b1e484953ef80f
PNik1ta <75731861+PNik1ta@users.noreply.github.com>
2022-02-09 22:16:12 +0400

    Add header with student info

--- src/main/resources/webapp/view/pages/students-page/student-main/app.js
+++ src/main/resources/webapp/view/pages/students-page/student-main/app.js
@@ -4,4 +4,13 @@ aside.addEventListener('mouseover', () => {
 })
 aside.addEventListener('mouseleave', () => {
 	aside.classList.remove('active');
+});
+
+let header = document.getElementsByClassName('header')[0];
+let profile = document.getElementsByClassName('profile')[0];
+let header_arrow = document.getElementsByClassName('header-arrow')[0];
+header_arrow.addEventListener('click', () => {
+	header.classList.toggle('active');
+	header_arrow.classList.toggle('active');
+	profile.classList.toggle('active');
 })
