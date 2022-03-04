5ea0afcfc7de318ee838d177e81802731e854eea
PNik1ta <75731861+PNik1ta@users.noreply.github.com>
2022-03-04 20:04:39 +0400

    Fix aside menu, header and footer in students page

--- src/main/resources/webapp/view/pages/students-page/app.js
+++ src/main/resources/webapp/view/pages/students-page/app.js
@@ -1,11 +1,3 @@
-let aside = document.getElementsByClassName('aside')[0];
-aside.addEventListener('mouseover', () => {
-	aside.classList.add('active');
-})
-aside.addEventListener('mouseleave', () => {
-	aside.classList.remove('active');
-});
-
 let header = document.getElementsByClassName('header')[0];
 let profile = document.getElementsByClassName('profile')[0];
 let header_arrow = document.getElementsByClassName('header-arrow')[0];
