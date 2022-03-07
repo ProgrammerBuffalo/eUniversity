484259218b0402f8186d9eb73adefc23d04d66c4
PNik1ta <75731861+PNik1ta@users.noreply.github.com>
2022-03-07 14:59:43 +0400

    Add admin-themelist page

--- src/main/resources/webapp/view/components/student-aside-menu/app.js
+++ src/main/resources/webapp/view/components/student-aside-menu/app.js
@@ -1,7 +1,6 @@
 let aside = document.getElementsByClassName('aside')[0];
 let burger = document.getElementsByClassName('burger')[0];
 
-console.log(burger);
 burger.addEventListener('click', () => {
 	aside.classList.toggle('active');
 	burger.classList.toggle('active');
